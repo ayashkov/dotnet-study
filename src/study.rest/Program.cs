9b24b722fed24ddd6864d14d9fc7b65c3eedb5bd
Alexei Yashkov <alexei.yashkov@yashkov.org>
2022-11-06 00:37:27 -0400

    Correct resource embedding

--- src/study.rest/Program.cs
+++ src/study.rest/Program.cs
@@ -7,15 +7,15 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// https://stackoverflow.com/questions/50915043/asp-net-core-2-1-serve-static-files-from-another-project
 // https://stackoverflow.com/questions/36198633/include-wwwroot-from-a-library-project
 app.UseStaticFiles(new StaticFileOptions {
     FileProvider = new EmbeddedFileProvider(
-        Assembly.Load(new AssemblyName("study.ui")), "study.ui")
+        Assembly.Load(new AssemblyName("study.ui")), "study.ui.wwwroot")
 });
+app.UseStaticFiles();
 app.UseRouting();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
-app.MapFallbackToFile("index.html");;
+app.MapFallbackToFile("index.html");
 app.Run();
