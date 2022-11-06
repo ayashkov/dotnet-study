130240ca48b002630b29ab78393b4a720b346b7f
Alexei Yashkov <alexei.yashkov@yashkov.org>
2022-11-05 21:20:08 -0400

    Use SDK build, add resources, set dependencies, remove legacy client

--- src/study.rest/Program.cs
+++ src/study.rest/Program.cs
@@ -1,24 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.FileProviders;
+using System.Reflection;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-}
-
-app.UseStaticFiles();
+// https://stackoverflow.com/questions/50915043/asp-net-core-2-1-serve-static-files-from-another-project
+// https://stackoverflow.com/questions/36198633/include-wwwroot-from-a-library-project
+app.UseStaticFiles(new StaticFileOptions {
+    FileProvider = new EmbeddedFileProvider(
+        Assembly.Load(new AssemblyName("study.ui")), "study.ui")
+});
 app.UseRouting();
-
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
-
 app.MapFallbackToFile("index.html");;
-
 app.Run();
