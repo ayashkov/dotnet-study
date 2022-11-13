aff79836b42944c65f3cc8686fca1c70a6021a09
Alexei Yashkov <alexei.yashkov@yashkov.org>
2022-11-13 12:27:28 -0500

    Rename to FilteredCopy

---
+++ src/study.build/FilteredCopy.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Build.Tasks;
+
+namespace study.build;
+
+public class FilteredCopy: Copy {
+    public override bool Execute()
+    {
+        Log.LogWarning("From FilteredCopy");
+
+        return base.Execute();
+    }
+}
